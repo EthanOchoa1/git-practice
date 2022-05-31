c4cb4ad9f28242263df5cea191d0634af8c6f720
Ethan Ochoa <ethanochoa@Ethans-MacBook-Pro.local>
2022-05-30 20:32:49 -0400

    covered loops and more git

---
+++ loops.js
@@ -0,0 +1,33 @@
+// Loops! Loops are good for doing something repeatedly, easy and convenient, Expensive and slow, and sometimes nessecary
+
+// Let i = 0 => initializes variable
+// i < 10 -> is the condition to be checked
+// i++ -> this adds 1 to i
+
+for(let i = 0; i < 10; i++){   // 0 = creating variable <10 = checking variable if its not more than 10 than 'i + 1' add one
+    // console.log('loop is running', i)
+}
+
+for(let i = 0; i <= 100; i++){
+    // step 1 -> i = 0 then i + 1
+    // step 2 -> i = 1 then i + 1
+    // step 3 -> i = 2 then i + 1
+    // step 4 -> i = 3 then i + 1
+    // console.log('testest', i)
+}
+//               0        1       2       3         4         5        6
+const cars = [`chevy`, `volvo`, `saab`, `ford`, `ferrari`, `tesla`, `nissan`]
+for(let i = 0; i < cars.length; i++){
+
+     if(i >= 4 && i < 6){
+        console.log(`here are my high end car -`, cars[i])
+        // console.log only high end cars
+    }
+    // console.log(`Checkout my car`, cars[i])
+}
+
+
+
+
+
+
